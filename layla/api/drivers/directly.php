b5fc42b8675590ae103ce80f910f84aba451246b
Koen Schmeets <k.schmeets@gmail.com>
2012-05-26 13:31:44 +0200

    Fixing directly API

--- layla/api/drivers/directly.php
+++ layla/api/drivers/directly.php
@@ -12,21 +12,23 @@ class Directly extends Driver {
 
 	public static function request($method, $segments, $data = array())
 	{
-		//Request::foundation()->query->replace($segments);
-
 		$method = strtoupper($method);
 		if(in_array($method, array('GET', 'POST', 'PUT', 'DELETE')))
 		{
  			Input::replace($data);
 		}
 
+		$config = static::config();
+		$_SERVER['PHP_AUTH_USER'] = $config['username'];
+		$_SERVER['PHP_AUTH_PW'] = $config['password'];
+
 		list($url, $uri, $query_string) = static::url($segments, $data);
 		$response = Route::forward($method, $uri);
 
 		$code = $response->foundation->getStatusCode();
 		$body = $response->content;
-		
-		return new Response($code, $body);
+
+		return new Response($code, json_decode($body));
 	}
 
 }
